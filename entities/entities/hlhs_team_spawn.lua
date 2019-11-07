0c1a75aa040999aa645595cbe6716106abbaf640
Stamos Prekas <prekasstamos@gmail.com>
2019-11-06 21:29:47 -0500

    much simpler brush

---
+++ entities/entities/hlhs_team_spawn.lua
@@ -0,0 +1,19 @@
+if SERVER then AddCSLuaFile() end
+ENT.Type = "brush"
+ENT.Base = "base_brush"
+
+function ENT:Initialize()
+	self:SetSolid(SOLID_BBOX)
+	self:SetTrigger(true)
+end
+
+function ENT:KeyValue(k, v)
+	if k == "team" then self.team = v end
+end
+
+function ENT:StartTouch(ply)
+	if ply:IsPlayer() and ply:Team() ~= self.team then
+		ply:PrintMessage(HUD_PRINTTALK, "Don't go in the enemies spawn!")
+		ply:Kill()
+	end
+end
